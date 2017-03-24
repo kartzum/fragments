43986dfad325918b8f789e423ac5dd07f1b4eb47
koval <r#cartoon1>
2017-03-24 11:24:55 +0300

    fragments. l_list. Refactoring.

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # Fragments of code (c/c++)
 
-## c-list
+## l_list
 Linked list.
 ### Links
-* (Linked lists)[http://www.learn-c.org/en/Linked_lists]
-* (Practical Programming in C)[https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-087-practical-programming-in-c-january-iap-2010/index.htm]
+* [Linked lists](http://www.learn-c.org/en/Linked_lists)
+* [Practical Programming in C](https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-087-practical-programming-in-c-january-iap-2010/index.htm)
 ### Make
 ```
 cmake_minimum_required(VERSION 3.6)
-project(c-list)
+project(l_list)
 
 set(CMAKE_C_STANDARD 11)
 
-set(SOURCE_FILES c-list/main.c)
-add_executable(c-list ${SOURCE_FILES})
+set(SOURCE_FILES l_list/main.c)
+add_executable(l_list ${SOURCE_FILES})
 ```
 
 ## d_list
 Doubly linked list.
 ### Links
-* (Doubly Linked List Program in C)[https://www.tutorialspoint.com/data_structures_algorithms/doubly_linked_list_program_in_c.htm]
+* [Doubly Linked List Program in C](https://www.tutorialspoint.com/data_structures_algorithms/doubly_linked_list_program_in_c.htm)
 ### Make
 ```
 cmake_minimum_required(VERSION 3.6)
---
+++ l_list/main.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+
+// l_list. Start.
+
+struct l_node {
+    int k;
+    int s;
+    void *data;
+    struct l_node *next;
+};
+
+struct l_list {
+    struct l_node *n;
+};
+
+struct l_list *l_list() {
+    struct l_list *r = malloc(sizeof(struct l_list));
+    r->n = NULL;
+    return r;
+}
+
+void l_list_add(struct l_list *l, int k, int s, void *data) {
+    struct l_node *result = malloc(sizeof(struct l_node));
+    result->k = k;
+    result->data = malloc(s);
+    result->s = s;
+    result->next = NULL;
+
+    if (l->n == NULL) {
+        l->n = result;
+    } else {
+        result->next = l->n;
+        l->n = result;
+    }
+
+    memcpy(result->data, data, s);
+}
+
+void l_list_for(struct l_list *l, void (*f)(struct l_node *i)) {
+    struct l_node *s = l->n;
+    while (s) {
+        (*f)(s);
+        s = s->next;
+    }
+}
+
+void l_list_printf(struct l_node *n) {
+    printf("%i, %s\n", n->k, n->data);
+}
+
+void l_list_free(struct l_list *l) {
+    struct l_node *s = l->n;
+    while (s) {
+        struct l_node *next = s->next;
+        free(s->data);
+        free(s);
+        s = next;
+    }
+    free(l);
+}
+
+// l_list. Finish.
+
+void l_list_for_test() {
+    struct l_list *l = l_list();
+    l_list_add(l, 1, 1, "1");
+    l_list_add(l, 2, 1, "2");
+    l_list_add(l, 3, 1, "3");
+    l_list_for(l, l_list_printf);
+    l_list_free(l);
+}
+
+int main() {
+    l_list_for_test();
+    return 0;
+}
