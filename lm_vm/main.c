4547ac9f894e50deda273bb27f7aaf06b8bb8c97
koval <r#cartoon1>
2017-03-29 13:15:47 +0300

    fragments. lm_vm. VM.

--- lm_vm/main.c
+++ lm_vm/main.c
@@ -39,10 +39,12 @@ struct l_s_code *l_s_code() {
 void l_s_code_free(struct l_s_code **c) {
     struct l_s_code *c_i = *c;
     struct l_s_node **d = c_i->data;
-    for (int i = 0; i < c_i->size; i++) {
-        if (d[i] != NULL) {
-            free(d[i]);
-            d[i] = NULL;
+    if (d != NULL) {
+        for (int i = 0; i < c_i->size; i++) {
+            if (d[i] != NULL) {
+                free(d[i]);
+                d[i] = NULL;
+            }
         }
     }
     free(*c);
@@ -96,6 +98,17 @@ void l_s_code_set(struct l_s_code *c, int i, int k, int v) {
     d[i] = node;
 }
 
+struct l_s_node *l_s_code_get_i(struct l_s_code *c, int i) {
+    struct l_s_node *result = NULL;
+    if (i >= 0 && i < c->size) {
+        struct l_s_node **d = c->data;
+        if (d != NULL) {
+            result = d[i];
+        }
+    }
+    return result;
+}
+
 void l_s_code_for(struct l_s_code *c, void (*f)(struct l_s_node *i)) {
     struct l_s_node **d = c->data;
     for (int i = 0; i < c->size; i++) {
@@ -112,25 +125,67 @@ void l_s_code_printf_c(struct l_s_node *n) {
 
 // code. Finish.
 
+// vm. Start.
+
+struct l_vm {
+    struct l_s_code *c;
+    struct l_code code;
+};
+
+struct l_vm *l_vm() {
+    struct l_vm *result = calloc(1, sizeof(struct l_vm));
+    result->c = l_s_code();
+    result->code = l_code();
+    return result;
+}
+
+void l_vm_free(struct l_vm **v) {
+    struct l_vm *v_i = *v;
+    l_s_code_free(&v_i->c);
+    free(*v);
+    *v = NULL;
+}
+
+void l_vm_exec(struct l_vm *v) {
+    int ip = 0;
+    struct l_s_node *s_node = l_s_code_get_i(v->c, ip);
+
+    while (s_node != NULL) {
+        if (s_node->k == v->code.iload) {
+        }
+        ip++;
+        s_node = l_s_code_get_i(v->c, ip);
+    }
+}
+
+// vm. Finish.
+
 // l. Finish.
 
-void l_code_test(bool l) {
+void l_s_code_test(bool trace) {
     struct l_s_code *c = l_s_code();
     struct l_code code = l_code();
     l_s_code_set(c, 0, code.iload, 0);
     l_s_code_set(c, 1, code.iload, 1);
-    if (l) {
+    if (trace) {
         l_s_code_for(c, l_s_code_printf_c);
     }
     l_s_code_free(&c);
-    free(c);
 }
 
-void l_test(bool l) {
-    l_code_test(l);
+void l_vm_test(bool trace) {
+    struct l_vm *v = l_vm();
+    l_vm_exec(v);
+    l_vm_free(&v);
+}
+
+void l_test(bool trace) {
+    l_s_code_test(trace);
+    l_vm_test(trace);
 }
 
 int main() {
-    l_test(false);
+    bool trace = true;
+    l_test(trace);
     return 0;
 }
