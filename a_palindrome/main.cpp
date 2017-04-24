21353e7b994bf5d4e9afa7009e556364c6ae2292
koval <r#cartoon1>
2017-04-24 10:21:41 +0300

    fragments. a_palindrome.

--- a_palindrome/main.cpp
+++ a_palindrome/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+bool a_palindrome(bool trace, string text) {
+    ulong i = 0;
+    ulong j = text.length() - 1;
+
+    while (i <= j) {
+        while (!((bool) isalpha(text[i])) && i <= j) {
+            i++;
+        }
+
+        while (!((bool) isalpha(text[j])) && j >= i) {
+            j--;
+        }
+
+        if (trace) {
+            cout << i << ", " << text[i] << ", " << j << ", " << text[j] << endl;
+        }
+
+        if (text[i] != text[j]) {
+            return false;
+        }
+
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
+void a_palindrome_test(bool trace) {
+    cout << std::boolalpha << (bool) a_palindrome(trace, "@@ ^fto ^^f )");
+    cout << endl;
+    cout << std::boolalpha << (bool) a_palindrome(trace, "   r!!ot $or ^");
+    cout << endl;
+}
 
 int main() {
-    std::cout << "!";
+    a_palindrome_test(true);
     return 0;
 }
